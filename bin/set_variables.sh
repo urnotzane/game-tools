194f5476854954a17204d490820ea7c1a8e53c1a
urnotzane <kaylezang@tencent.com>
2023-12-19 15:31:35 +0800

    fix: 设置github env

--- bin/set_variables.sh
+++ bin/set_variables.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # 读取package.json文件并提取version字段的值
-VERSION="v$(cat package.json | jq -r '.version')"
+VERSION=$(cat package.json | jq -r '.version')
 
 # 获取最新的git tag
 LATEST_TAG=$(git describe --tags --abbrev=0)
 
 # 比较VERSION和LATEST_TAG，如果它们不相等，则设置TAG_UPDATED为TRUE
-if [ "$VERSION" != "$LATEST_TAG" ]; then
-  TAG_UPDATED='true'
+if [ "v$VERSION" != "$LATEST_TAG" ]; then
+  TAG_UPDATED=true
 else
-  TAG_UPDATED='false'
+  TAG_UPDATED=false
 fi
 
 APP_NAME="Game Tools_"$VERSION"_x64"
 
+echo $LATEST_TAG
+
 # 输出VERSION和TAG_UPDATED的值
-echo "VERSION: $VERSION"
-echo "TAG_UPDATED: $TAG_UPDATED"
-echo "APP_NAME: $APP_NAME"
+echo "VERSION=$VERSION" >> $GITHUB_ENV
+echo "TAG_UPDATED=$TAG_UPDATED" >> $GITHUB_ENV
+echo "APP_NAME=$APP_NAME" >> $GITHUB_ENV
