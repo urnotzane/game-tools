0cf1f93dc78cd99bd88ccd25809b1c3aaf0d3e47
urnotzane <kaylezang@tencent.com>
2023-12-19 11:15:36 +0800

    feat: rust build 缓存

--- bin/get_version.sh
+++ bin/get_version.sh
@@ -8,11 +8,14 @@ LATEST_TAG=$(git describe --tags --abbrev=0)
 
 # 比较VERSION和LATEST_TAG，如果它们不相等，则设置TAG_UPDATED为TRUE
 if [ "$VERSION" != "$LATEST_TAG" ]; then
-  TAG_UPDATED="true"
+  TAG_UPDATED=true
 else
-  TAG_UPDATED="false"
+  TAG_UPDATED=false
 fi
+
+APP_NAME="Game Tools_"$VERSION"_x64"
 
 # 输出VERSION和TAG_UPDATED的值
 echo "VERSION: $VERSION"
 echo "TAG_UPDATED: $TAG_UPDATED"
+echo "APP_NAME: $APP_NAME"
