af941117556e18633a2ed936a3b0ca30c7e6e0d6
urnotzane <kaylezang@tencent.com>
2023-12-19 10:36:31 +0800

    feat: 新增git action

---
+++ bin/get_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 读取package.json文件并提取version字段的值
+VERSION=$(cat package.json | jq -r '.version')
+
+# 获取最新的git tag
+LATEST_TAG=$(git describe --tags --abbrev=0)
+
+# 比较VERSION和LATEST_TAG，如果它们不相等，则设置TAG_UPDATED为TRUE
+if [ "$VERSION" != "$LATEST_TAG" ]; then
+  TAG_UPDATED="true"
+else
+  TAG_UPDATED="false"
+fi
+
+# 输出VERSION和TAG_UPDATED的值
+echo "VERSION: $VERSION"
+echo "TAG_UPDATED: $TAG_UPDATED"
