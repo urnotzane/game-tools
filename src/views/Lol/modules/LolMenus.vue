0ff4f8f02ff39f6746d05987aceb09d7ca4b6bdb
urnotzane <kaylezang@tencent.com>
2024-05-23 16:24:23 +0800

    fix: 解决tailwind与antd样式冲突问题

--- src/views/Lol/modules/LolMenus.vue
+++ src/views/Lol/modules/LolMenus.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative h-full py-4 px-6">
-    <a-card title="英雄联盟" class="h-full" :extra="lolInitialStore.clientUrl" :bodyStyle="{ height: '100%' }">
+    <a-card title="英雄联盟" class="h-full" :bodyStyle="{ height: '100%' }">
       <a-card-grid class="!w-[25%] !h-[25%] text-xl text-center flex flex-col justify-center items-center">
-        <div><a-button type="primary" @click="copyToClipboard(lolInitialStore.clientToken)">复制Token</a-button></div>
+        <div><a-button @click="copyToClipboard(lolInitialStore.clientToken)">复制Token</a-button></div>
         <div class="mt-3"><a-button type="primary" @click="copyToClipboard(lolInitialStore.clientUrl)">复制接口地址</a-button>
         </div>
       </a-card-grid>
